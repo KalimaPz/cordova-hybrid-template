1d1182fa82d1f7cf2bd47065ba4ea2c94e92e07d
Donnukrit Satirakul <donnukrit.s@digital.organic>
2021-11-30 15:35:03 +0700

    initial commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <p>{{ this.$store.state.count }}</p>
+      <p><button @click="inc">count+</button></p>
+      <p><button @click="dec">count-</button></p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+    name : "Counter",
+    methods: {
+        inc() {
+            this.$store.state.count++;
+        },
+         dec() {
+            this.$store.state.count--;
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
